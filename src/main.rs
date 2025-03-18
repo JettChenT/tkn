e148ba09b8ab0972912def06dc4ca4afd4a610ce
Jett Chen <jettchen12345@gmail.com>
2025-03-18 11:15:17 +0800

    Implement cost calculation and improve token stats display

--- src/main.rs
+++ src/main.rs
@@ -12,9 +12,11 @@ struct Args {
     path: Option<PathBuf>,
 }
 
+#[derive(Debug)]
 struct TokenStats {
     total_tokens: usize,
-    cost_dollars: Option<f64>,
+    cost_dollars: f64,
+    cost_cached_dollars: f64,
 }
 
 struct ModelCostInfo {
@@ -33,15 +35,36 @@ enum TokenizerType {
 impl TokenizerType {
     fn as_hf(&self) -> String {
         match &self {
-            TokenizerType::Gpt4o => "Xenova/gpt-4o".to_string(),
-            TokenizerType::Gemini => "Xenova/gemma-2-tokenizer".to_string(),
-            TokenizerType::Claude_3_7 => "Xenova/claude-tokenizer".to_string(),
-            TokenizerType::Claude_3_5 => "Xenova/claude-tokenizer".to_string(),
+            Self::Gpt4o => "Xenova/gpt-4o".to_string(),
+            Self::Gemini => "Xenova/gemma-2-tokenizer".to_string(),
+            Self::Claude_3_7 => "Xenova/claude-tokenizer".to_string(),
+            Self::Claude_3_5 => "Xenova/claude-tokenizer".to_string(),
         }
     }
 
     fn cost(&self) -> ModelCostInfo {
-        todo!()
+        match &self {
+            Self::Gpt4o => ModelCostInfo {
+                input_cost: 2.5,
+                output_cost: 10.0,
+                cached_input_cost: 1.25,
+            },
+            Self::Gemini => ModelCostInfo {
+                input_cost: 0.10,
+                output_cost: 0.40,
+                cached_input_cost: 0.025,
+            },
+            Self::Claude_3_7 => ModelCostInfo {
+                input_cost: 3.,
+                output_cost: 15.,
+                cached_input_cost: 0.3,
+            },
+            Self::Claude_3_5 => ModelCostInfo {
+                input_cost: 3.,
+                output_cost: 15.,
+                cached_input_cost: 0.3,
+            },
+        }
     }
 }
 
@@ -51,9 +74,14 @@ fn calc_stats(content: &String, tokenizer_type: TokenizerType) -> Result<TokenSt
     let tokens = tokenizer
         .encode_fast(content.as_str(), false)
         .map_err(|e| eyre!("Failed to encode text: {}", e))?;
+    let cost_info = tokenizer_type.cost();
+    let tok_count = tokens.len();
+    let tok_mils = (tok_count as f64) / 1_000_000.;
+
     Ok(TokenStats {
-        total_tokens: tokens.len(),
-        cost_dollars: None,
+        total_tokens: tok_count,
+        cost_dollars: cost_info.input_cost * tok_mils,
+        cost_cached_dollars: cost_info.cached_input_cost * tok_mils,
     })
 }
 
@@ -69,20 +97,20 @@ fn main() -> Result<()> {
     };
 
     println!(
-        "GPT-4o token Count: {:?}",
-        calc_stats(&content, TokenizerType::Gpt4o)?.total_tokens
+        "GPT-4o stats: {:#?}",
+        calc_stats(&content, TokenizerType::Gpt4o)?
     );
     println!(
-        "Gemini (est. via gemma 2) token Count: {:?}",
-        calc_stats(&content, TokenizerType::Gemini)?.total_tokens
+        "Gemini (est. via gemma 2) stats: {:#?}",
+        calc_stats(&content, TokenizerType::Gemini)?
     );
     println!(
-        "Claude 3.7 token Count: {:?}",
-        calc_stats(&content, TokenizerType::Claude_3_7)?.total_tokens
+        "Claude 3.7 stats: {:#?}",
+        calc_stats(&content, TokenizerType::Claude_3_7)?
     );
     println!(
-        "Claude 3.5 token Count: {:?}",
-        calc_stats(&content, TokenizerType::Claude_3_5)?.total_tokens
+        "Claude 3.5 stats: {:#?}",
+        calc_stats(&content, TokenizerType::Claude_3_5)?
     );
     Ok(())
 }
